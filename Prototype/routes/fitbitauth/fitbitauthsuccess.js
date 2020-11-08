e04812c4ed85f472734c14423ceef57e73f72cda
ZhixingZhao <56173726+ZhixingZhao@users.noreply.github.com>
2020-11-07 19:11:57 -0500

    Prototype

---
+++ Prototype/routes/fitbitauth/fitbitauthsuccess.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const fetch = require('node-fetch')
+const router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+//need to get data and send to render
+});
+
+
+module.exports = router;
