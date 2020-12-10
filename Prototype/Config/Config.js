297c6c6d69fdf0853fe9be17a6e12567ac266e82
ZhixingZhao <56173726+ZhixingZhao@users.noreply.github.com>
2020-12-10 18:35:35 -0500

    Minor Changes (not part of group project)

---
+++ Prototype/Config/Config.js
@@ -0,0 +1,9 @@
+let fitbitconfig = {
+    FITBIT_CLIENT_ID: '22C378',
+    FITBIT_CLIENT_SECRET: '92c6f9badf16cc372811937ca7addd22'
+}
+let googlefitconfig = {
+    GOOGLEFIT_CLIENT_ID: '309367663683-c2di8t92i7mcfct0bfh357voesn5hhji.apps.googleusercontent.com',
+    GOOGLEFIT_CLIENT_SECRET: 'hI9fUA3DCRVjJo6c3ugJ1VJS'
+}
+module.exports = {fitbitconfig, googlefitconfig}
--- Ps4/routes/ps4.js
+++ Ps4/routes/ps4.js
@@ -5,6 +5,15 @@ const fetchoption = require('../Config/config')
 const Convert = require('../ConvertFunction');
 
 
+router.options("/", function (req, res, next) {
+    res.writeHead(200, {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*",
+    });
+    res.end();
+});
+
 
 const getData = async (CountryCode) => {
     let rawCovidData = await fetch(fetchoption.fetchoptions.url + CountryCode);
@@ -12,16 +21,22 @@ const getData = async (CountryCode) => {
     return cleanCovidData;
 }
 
-router.post('/', (req, res, next) => {
-    let CountryName = req.body.CountryName
+router.get('/', (req, res, next) => {
+    // console.log(req.params)
+    // console.log(req.CountryName)
+    let CountryName = req.query.CountryName
     let CountryCode = Convert.getCountryCode(CountryName)
     getData(CountryCode).then(data => {
-//        console.log(data.data)
-        res.render('CovidDataRender', data.data)
+        // console.log(data.data)
+        // res.render('CovidDataRender', data.data)
+        console.log(CountryName)
+        res.json(data.data)
+
 
     });
 
 });
+
 
 
 module.exports = router;
